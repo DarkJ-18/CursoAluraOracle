0a7600d81f813013a94ecf03671941a84c6e9ea5
DarkJ-18 <j.juancamilojurado@gmail.com>
2024-07-02 23:35:54 -0500

    Se sube lo trabajado en el curso

---
+++ Desafios/condicionales/1.js
@@ -0,0 +1,5 @@
+let i = 1;
+while (i <= 9){
+    i = i+1;
+}
+console.log(i);
---
+++ Desafios/condicionales/3.js
@@ -0,0 +1,7 @@
+let num =prompt("Ingresa un numero ");
+let contador = 0;
+while( num >= contador){
+    alert(contador)
+    contador++;
+    
+}
