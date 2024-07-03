8e0909c442aad32ce0800e6b00f0b4c0d49dced0
DarkJ-18 <j.juancamilojurado@gmail.com>
2024-07-02 23:43:57 -0500

    Subiendo challenger 1

--- numeroSecreto/app.js
+++ numeroSecreto/app.js
@@ -3,8 +3,6 @@ let intentos = 0;
 let listaNumerosSorteados = [];
 let numeroMaximo = 10;
 
-
-
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
